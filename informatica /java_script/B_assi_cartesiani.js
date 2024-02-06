66515690a25e47f13efa688d1b847d0f8ff3c1a6
Lidiaaa06 <lidy.06a@gmail.com>
2024-02-06 21:21:42 +0000

    .

--- informatica /java_script/B_assi_cartesiani.js
+++ informatica /java_script/B_assi_cartesiani.js
@@ -1,10 +1,7 @@
 m = parseInt(prompt("inserisci coefficiente m della retta y=mx+c"));
 c = parseInt(prompt("inserisci coefficiente c della retta y=mx+c"));
 
-
-intersezioneAsseX = (0 - c) / m;
-
-console.log("Coordinata Y per ascissa -1 = " + calcolaY(-1));
-console.log("Coordinata Y per ascissa 10 = " + calcolaY(10));
-console.log("Punto di intersezione con l'asse Y = " + calcolaY(0));
-console.log("Punto di intersezione con l'asse X = " + intersezioneAsseX);
+console.log("Coordinata Y per ascissa -1 = " + (m * x) + c);
+console.log("Coordinata Y per ascissa 10 = " + (m * x) + c);
+console.log("Punto di intersezione con l'asse Y = " + (m * x) + c);
+console.log("Punto di intersezione con l'asse X = " + (0 - c) / m);
