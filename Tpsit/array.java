cc1bdda18a2d8f78ae963eb83fb42a0532ad250c
Lidiaaa06 <lidy.06a@gmail.com>
2024-02-21 15:24:42 +0000

    commit

--- Tpsit/array.java
+++ Tpsit/array.java
@@ -3,24 +3,77 @@ public class array
 {
 public static void main(String args[]) throws NumberFormatException, IOException
 {
-int i=0;
-int v[]=new int[10];
-InputStreamReader In = new InputStreamReader(System.in);
-BufferedReader Tastiera = new BufferedReader(In);
-for (i=0; i<10; i++)
-{
-System.out.print("Valore: v["+i+"]= ");
-v[i]=Integer.parseInt(Tastiera.readLine());
+    somma1();
+    somma();
+    AdiK();
 }
-for(i=0; i<10;i++)
+
+public static void somma1()throws NumberFormatException, IOException 
 {
-System.out.println("v["+i+"]="+v[i]);
+    int i=0;
+    int v[]=new int[10];
+    InputStreamReader In = new InputStreamReader(System.in);
+    BufferedReader Tastiera = new BufferedReader(In);
+    for (i=0; i<10; i++)
+    {
+    System.out.print("Valore: v["+i+"]= ");
+    v[i]=Integer.parseInt(Tastiera.readLine());
+    }
+    for(i=0; i<10;i++)
+    {
+    System.out.println("v["+i+"]="+v[i]);
+    }
+    int S=0;
+    for(i=0; i<10;i++)
+    {
+    S=S+v[i];
+    }
+    System.out.println("somma="+S);
+
 }
-int S=0;
-for(i=0; i<10;i++)
+public  static void somma()throws NumberFormatException, IOException 
 {
-S=S+v[i];
+    int x;
+    InputStreamReader In = new InputStreamReader(System.in);
+    BufferedReader Tastiera = new BufferedReader(In);
+    do
+    {
+    System.out.print("Immettere un numero: ");
+    x=Integer.parseInt(Tastiera.readLine());
+    } while (x<=0);
+    System.out.println("numero inserito: " + x);
+    int i=0;
+    int v[]=new int[x];
+    for (i=0; i<x; i++)
+    {
+    System.out.print("Valore: ");
+    v[i]=Integer.parseInt(Tastiera.readLine());
+    }
+
+    for(i=0; i<x;i++)
+    {
+    System.out.println("v["+i+"]="+v[i]);
+    }
+    int S=0;
+    for(i=0; i<x;i++)
+    {
+    S=S+v[i];
+    }
+    System.out.println("somma="+S);
 }
-System.out.println("somma="+S);
+
+
+public static void AdiK() 
+{
+    double[] A = {2 ,3,-4,5,-6};
+
+    int c=0,i=0;
+
+    for( i=0;i<A.length; i++)
+    {
+        if(A[i]<0)   c++;
+    }
+
+    System.out.println("numeri minori di 0= "+c);
 }
 }
