60035d40713e6b5ece420bbf458cba4decd1a6a5
Lidiaaa06 <lidy.06a@gmail.com>
2024-02-28 12:28:28 +0000

    .

--- informatica /Pila/Pila.cpp
+++ informatica /Pila/Pila.cpp
@@ -7,5 +7,28 @@ Pila::Pila()
     vet=new int[10];
     size=10;
     fine=0;
+}
+
+Pila::Pila(int size)
+{
+    vet=new int [10];
+    this->size=size;
+    fine=0;
+}
+
+bool Pila::isEmpty()
+{
+    if(fine=0)return true;
+    return false;
+}
+
+bool Pila::isFull()
+{
+    if(fine==size)return true;
+    return false;
+}
+
+bool Pila::push(int x)
+{
     
 }
