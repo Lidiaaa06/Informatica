c8c82af6c2d68babce3154debd140a1b0ee74d71
Lidiaaa06 <lidy.06a@gmail.com>
2024-09-25 13:51:13 +0000

    .

--- Informatica5/Es01/Es_A_Tavola_pitagorica.php
+++ Informatica5/Es01/Es_A_Tavola_pitagorica.php
@@ -10,7 +10,8 @@ for($i=0; $i<10; $i++)
 {
      for($j=0; $j<10; $j++) 
      {
-          echo "<tr><td>$i</td></tr>";
+          $c = $i * $j;  
+          echo "<tr><td>$c</td></tr>";
      }
      
 }
